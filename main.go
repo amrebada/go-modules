4d225b0dbea0150019c82435482d339d43c25006
amrebada <amr.app.engine@gmail.com>
2023-02-14 00:01:19 +0400

    chore: remove unused test code

--- main.go
+++ main.go
@@ -1,47 +1,33 @@
 package main
 
 import (
-	// "flag"
-	"encoding/json"
+	"flag"
 	"fmt"
 
 	"github.com/amrebada/go-modules/core"
-	// "github.com/amrebada/go-modules/modules"
-	"github.com/amrebada/go-modules/modules/user"
+	"github.com/amrebada/go-modules/modules"
 )
 
 func main() {
-	RegisterDTO := user.RegisterDto{}
-	StringDTO := ""
-	ArrayRegisterDTO := []user.RegisterDto{}
-	schemas := core.Schemas{}
-	schemas = core.ConvertToSchema(RegisterDTO, schemas)
-	schemas = core.ConvertToSchema(StringDTO, schemas)
-	schemas = core.ConvertToSchema(ArrayRegisterDTO, schemas)
-	data, err := json.MarshalIndent(schemas, "", "  ")
+
+	isMigrate := false
+	isSwagger := false
+	env := "dev"
+
+	flag.BoolVar(&isMigrate, "m", false, "auto migrate database")
+	flag.BoolVar(&isSwagger, "sw", false, "generate swagger")
+	flag.StringVar(&env, "env", "dev", "identify which environment to load from {.env} file [.env.prod, .env.dev, .env.test, .env]")
+	flag.Parse()
+	core.NewConfig(core.Stage(env)).
+		SetMigrate(isMigrate).
+		SetSwagger(isSwagger)
+
+	app := core.NewServer()
+	app.MainModule = modules.NewAppModule()
+	app.RegisterMainModule()
+	err := app.Start()
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(string(data))
-
-	// isMigrate := false
-	// isSwagger := false
-	// env := "dev"
-
-	// flag.BoolVar(&isMigrate, "m", false, "auto migrate database")
-	// flag.BoolVar(&isSwagger, "sw", false, "generate swagger")
-	// flag.StringVar(&env, "env", "dev", "identify which environment to load from {.env} file [.env.prod, .env.dev, .env.test, .env]")
-	// flag.Parse()
-	// core.NewConfig(core.Stage(env)).
-	// 	SetMigrate(isMigrate).
-	// 	SetSwagger(isSwagger)
-
-	// app := core.NewServer()
-	// app.MainModule = modules.NewAppModule()
-	// app.RegisterMainModule()
-	// err := app.Start()
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
 
 }
